c9483fe104b0a40a3fc1705c345b3de4200cf3ca
Patrick Bowman <patrickbowman@MacBook-Pro-2.askey.com>
2023-04-19 21:26:41 -0700

    Cleaned out comments, commented out debug stuff, still works as needed.

--- World_Clock.R
+++ World_Clock.R
@@ -74,36 +74,37 @@ fill_color <- function(fc_dn)
 # and shows a world clock with four dials, one each for Los Angeles, DC, London, and Tokyo
 #
 # Dials are shaded if the time there is between 6PM and 6AM.
-# Note that the times may be on a different day, and that is not currently shown (though can be inferred)
+# Note that the times may be on a different day.
+# Above each clock is the day of the month in that locatino
 #
-world_clock <- function(current_time = Sys.time(), show_tzones = c("US/Pacific","US/Eastern","Europe/Moscow",'Asia/Tokyo'))
+world_clock <- function(current_time = Sys.time(), show_tzones = c("US/Pacific","US/Eastern","Europe/London",'Asia/Tokyo'), use_tzones=show_tzones)
 {
-  # show_tzones isn't used for anything...yet. Just getting the parameter in there and working
-  #get timezones that have a slash but don't start with Et (Etc, a bunch of GMT relative ones that I don't way)
+  # show_tzones is a list of timezones to use, with the given defaults. 
+  # use_tzones isn't used yet but is intended to be for showing a different timezone than one of the standard ones
+  #
+  # The first line here gets timezones that have a slash but don't start with Et (Etc, a bunch of GMT relative ones that I don't way)
   #  Works by allowing anything that starts with something that's NOT an e and eventually has a slash
   #  OR by allowing anything that DOES start with an E but does NOT have a lower-case t after it, followed by anything and a slash
   # I think it's uglier than it should be, ut it works.
-  # Takes a time value as the starting point, uses system time if none given
-  # It's the "current time" so everything gets adjusted as if it's starting from Los Angeles
-# but so far not using this for anything....  
+ # but so far not using this for anything....  
 main_tz <- OlsonNames()[grep('^[^E].+/|^[worlE][^t].+/',OlsonNames())]
 
 # The initial time, whether input or Sys.time, will be converted to local
 # Which was fine at first, but also need to show it specifically as LA time
 # So these current_* variables can probably go at some point
-current_hour <- hour(current_time) #format(current_time,'%H')
-current_minute <- minute(current_time) #format(current_time,'%M')
-
-la_time <- with_tz(current_time, tzone = "America/Los_Angeles")
-lon_time <-with_tz(current_time, tzone = "Europe/London")
-ny_time <-with_tz(current_time, tzone = "US/Eastern")
-jp_time <- with_tz(current_time,tzone='Asia/Tokyo')
-mo_time <- with_tz(current_time,tzone='Europe/Moscow')
+# current_hour <- hour(current_time) #format(current_time,'%H')
+# current_minute <- minute(current_time) #format(current_time,'%M')
+# 
+# la_time <- with_tz(current_time, tzone = "America/Los_Angeles")
+# lon_time <-with_tz(current_time, tzone = "Europe/London")
+# ny_time <-with_tz(current_time, tzone = "US/Eastern")
+# jp_time <- with_tz(current_time,tzone='Asia/Tokyo')
+# mo_time <- with_tz(current_time,tzone='Europe/Moscow')
 show_times <- list(length(show_tzones))
 show_hours <- list(length(show_tzones))
 for (tzs in show_tzones )
 {
-  print(tzs)
+ # print(tzs) # 
   #show_times<- with_tz(current_time, tzone = tzs)
   #show_hours<- hour(show_times) #hour(with_tz(current_time, tzone = tzs))
   
@@ -118,10 +119,10 @@ for (tzs in show_tzones )
   #show_times[tzs]= c(show_times, with_tz(current_time, tzone = tzs))
   #print (show_times[tzs])
 }
-print(show_times)
+#print(show_times)
 #print(show_hours)
 
-all_tz <- c('Europe/London',"EST","Europe/Moscow","Asia/Tokyo")
+#all_tz <- c('Europe/London',"EST","Europe/Moscow","Asia/Tokyo")
 
 # Sys.timezone()
 # tz_df <-data.frame(time_zone =Sys.timezone(),current_time, current_hour, current_minute)
@@ -133,7 +134,7 @@ all_tz <- c('Europe/London',"EST","Europe/Moscow","Asia/Tokyo")
 #   rbind(tz_df, c(tz,tz_time,hour(tz_time), minute(tz_time)  ))
 # }
 
-dc_time <- ny_time
+#dc_time <- ny_time
 
 #
 # okay, we have it finding the time, and calculating for other time zones
@@ -146,19 +147,17 @@ dc_time <- ny_time
 # Cycle through all of them and there you go.
 # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
-clock_df <- data.frame(
- tz =show_tzones,  #c("PST","EST","GMT","JST"),
- tz_time= show_times, #c(la_time,dc_time, lon_time,jp_time),
-  tzhour=  c(hour(la_time),hour(dc_time), hour(lon_time),hour(jp_time)),#show_hours,  #
-  tzmin=c(minute(la_time), minute(dc_time), minute(lon_time), minute(jp_time)), #show_minutes, #
-  tzday=c(day(la_time), day(dc_time), day(lon_time), day(jp_time)),#show_days, #
- tz_dn= c(day_night(la_time), day_night(dc_time), day_night(lon_time),day_night(jp_time))
-)
+# clock_df <- data.frame(
+#  tz =show_tzones,  #c("PST","EST","GMT","JST"),
+#  tz_time= show_times, #c(la_time,dc_time, lon_time,jp_time),
+#   tzhour=  c(hour(la_time),hour(dc_time), hour(lon_time),hour(jp_time)),#show_hours,  #
+#   tzmin=c(minute(la_time), minute(dc_time), minute(lon_time), minute(jp_time)), #show_minutes, #
+#   tzday=c(day(la_time), day(dc_time), day(lon_time), day(jp_time)),#show_days, #
+#  tz_dn= c(day_night(la_time), day_night(dc_time), day_night(lon_time),day_night(jp_time))
+# )
 #print(show_hours)
-print
-#clock_df # use this to see what's in the data frame, where needed
-# Need to clean some of these up, helpful though they are
-#day(clock_df$tz_time[4])
+
+
 #
 # Start displaying the clock faces
 #
@@ -167,107 +166,19 @@ grid.newpage()
 vp <- viewport(width=1.0, height=0.5) # originally four times as high, now twice as high
 pushViewport(vp)
 grid.rect(gp=gpar(col="black")) #black around all 
-nrow(clock_df)  #number of records, for if/we we want to automate number of clocks
-# Square inside the main rectangled, 1/4 the width and 1/2 the height
-# pushViewport(viewport(x=0.0, width=0.25, height=0.5,
-#                       just="left", name=clock_df$tz[1])) #this makes the name the timezone - previously A B C D, and maybe that was better.
-# grid.rect(gp=gpar(col="red",fill=fill_color(clock_df$tz_dn[1])))
-# #upViewport(2)
-# #pushViewport(viewport(x=0.0, width=0.25, height=0.25,
-# #                      just="left", name="header1"))
-# #grid.rect(gp=gpar(col="purple",fill=fill_color(clock_df$tz_dn[1])))
-# #grid.circle(x=0.125, y=0.5, default="native", 
-# #            r=unit(0.5, "native"))
-# drawClock(hour = clock_df$tzhour[1], minute = clock_df$tzmin[1])
-# grid.text(clock_df$tz[1])
-# 
-# upViewport()
-# #upViewport(2)
-# pushViewport(viewport(x=0.0, width=0.25, height=0.25,y=0.825,
-#                       just="left", name="header1"))
-# grid.text(clock_df$tzday[1])
-# upViewport()
-# # upViewport(3)
-# # pushViewport(viewport(x=0.0, width=0.25, height=0.25,
-# #                       just="left", name="header1")
-# #              )
-# # grid.rect(gp=gpar(col="cyan"))
-# # #grid.text(clock_df$tzday)
-# # grid.text('17') # so this isn't showing,  nor the border
-# # It seems to still be part of the first clock viewport, not the one above it.
-# #upViewport(1)
-# #
-# # Second clock face starts here
-# #
-# pushViewport(viewport(x=0.25, width=0.25, height=0.5,
-#                       just="left", name=clock_df$tz[2]))
-# grid.rect(gp=gpar(col="green",fill=fill_color(clock_df$tz_dn[2])))
-# drawClock(hour = clock_df$tzhour[2], minute = clock_df$tzmin[2])
-# grid.text(clock_df$tz[2])
-# upViewport()
-# pushViewport(viewport(x=0.25, width=0.25, height=0.25,y=0.825,
-#                       just="left", name="header2"))
-# grid.text(clock_df$tzday[2])
-# upViewport()
-# 
-# #
-# # Third clock face starts here
-# #
-# pushViewport(viewport(x=0.5, width=0.25, height=0.5,
-#                       just="left", name=clock_df$tz[3]))
-# grid.rect(gp=gpar(col="yellow",fill=fill_color(clock_df$tz_dn[3])))
-# drawClock(hour = clock_df$tzhour[3], minute = clock_df$tzmin[3])
-# grid.text(clock_df$tz[3])
-# upViewport()
-# pushViewport(viewport(x=0.5, width=0.25, height=0.25,y=0.825,
-#                       just="left", name="header3"))
-# grid.text(clock_df$tzday[3])
-# upViewport()
-# #
-# # Fourth clock face starts here
-# #
-# pushViewport(viewport(x=0.75, width=0.25, height=0.5,
-#                       just="left", name=clock_df$tz[4]))
-# grid.rect(gp=gpar(col="blue",fill=fill_color(clock_df$tz_dn[4])))
-# drawClock(hour = clock_df$tzhour[4], minute = clock_df$tzmin[4])
-# grid.text(clock_df$tz[4])
-# upViewport()
-# pushViewport(viewport(x=0.75, width=0.25, height=0.25,y=0.825,
-#                       just="left", name="header4"))
-# grid.text(clock_df$tzday[4])
-# upViewport()
+#nrow(clock_df)  #number of records, for if/we we want to automate number of clocks
 
-# for (i in 1:4)
-# {
-# #print(i)
-# 
-#   pushViewport(viewport(x=0.25*(i-1), width=0.25, height=0.5,
-#                         just="left", name=clock_df$tz[i]))
-#   grid.rect(gp=gpar(col="blue",fill=fill_color(clock_df$tz_dn[i])))
-#   drawClock(hour = clock_df$tzhour[i], minute = clock_df$tzmin[i])
-#   #grid.text(clock_df$tz[i])
-#   upViewport()
-#   pushViewport(viewport(x=0.25*(i-1), width=0.25, height=0.25,y=0.825,
-#                         just="left", name=paste0("header",i)))
-#   grid.text(clock_df$tzday[i])
-#   upViewport()
-#   pushViewport(viewport(x=0.25*(i-1), width=0.25, height=0.25,y=0.125, #.825,
-#                         just="left", name=paste0("footer",i)))
-#   grid.text(clock_df$tz[i]) #, gp=gpar(font_size=20))
-#   upViewport()
-# }
 
-n<-0
+n<-0 #Probably a cleaner way to use both time zones and a count, but it works.
 for (tz in show_tzones)
 {
   n<-n+1
-  print(tz)
+ # print(tz)
 #print(show_times[[i]]$hour) show_times[[tzs]]$dn
   pushViewport(viewport(x=0.25*(n-1), width=0.25, height=0.5,
-                        just="left", name=clock_df$tz[tz]))
+                        just="left", name=paste0("clock",n)))
   grid.rect(gp=gpar(col="blue",fill=fill_color(show_times[[tz]]$dn)))
   drawClock(hour = show_times[[tz]]$hour, minute = show_times[[tz]]$minute)
-  #grid.text(clock_df$tz[i])
   upViewport()
   pushViewport(viewport(x=0.25*(n-1), width=0.25, height=0.25,y=0.825,
                         just="left", name=paste0("header",n)))
@@ -278,16 +189,26 @@ for (tz in show_tzones)
   grid.text(tz) #, gp=gpar(font_size=20))
   upViewport()
 }
-#grid.rect(gp=gpar(col="green"))
-#world_clock() # gets current local time and adjusts for the others
-#world_clock(as.POSIXlt("2023-03-31 09:34:00")) # treats input as local time
-# world_clock(as.POSIXlt("2023-03-31 08:15:00", tz="Asia/Tokyo")) #puts input into that timezone
+
+# world_clock() # gets current local time and adjusts for the others
+# world_clock(as.POSIXlt("2023-03-31 09:34:00")) # treats input as local time
+# world_clock(as.POSIXlt("2023-03-31 08:15:00", tz="Asia/Tokyo")) # puts input into that timezone
 # world_clock(as.POSIXlt("1945-09-02 08:15:00", tz="Asia/Tokyo")) # and this one works, well enough.
 # so next steps are
 # 1. Get the clock faces more automatic - done
-# so we just iterate through the same code for 1, 2, 3, 4
+# so we just iterate through the same code for 1, 2, 3, 4 - Done
 # 2. Show month along with date in the heeader
-# 3. Set up a footer and put the complete time zone in there
-# 4. Do a lookup for the three letter timezones so I can use them in the center consistently, still
+# 3. Set up a footer and put the complete time zone in there - Done
+# 4. Do a lookup for the three letter timezones so I can use them in the center consistently, still - no longer needed
 # 5. Add a parameter with a vector of timezones and show clocks for each one in there.
+# 6. Have a way to easily include e.g. Tinian and Los Alamos instead of Guam and Denver
+# IF I put in "Tinian" and can later transalate that as being Guam
+# Then I can display what I put in and generate times based on Guam (etc.)
+#
+#Current time, with Moscow instead of London
+#world_clock(,c("US/Pacific","US/Eastern","Europe/Moscow",'Asia/Tokyo'))
+#Surrender, starting from Tokyo, standard zones
+#world_clock(as.POSIXlt("1945-09-02 08:15:00", tz="Asia/Tokyo")) 
+# and with time zones specified
+#world_clock(as.POSIXlt("1945-09-02 08:15:00", tz="Asia/Tokyo"),c("US/Pacific","US/Eastern","Europe/Moscow",'Asia/Tokyo')) 
 }
